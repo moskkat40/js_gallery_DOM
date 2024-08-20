fe2daaef8e269e8df8fa7ee9763d30ed93484d9e
Kateryna <moskkat40@gmail.com>
2024-08-20 18:56:54 +0200

    make build

---
+++ index.a4ceecb1.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){e.preventDefault(),e.target.closest(".list-item__link")&&(t.src=e.target.closest(".list-item__link").href)});
+//# sourceMappingURL=index.a4ceecb1.js.map
